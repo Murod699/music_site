272ab27764d39f18ce20f16f60a47ee9f3f2db90
Murodjon <murodbek.0338@gmail.com>
2021-03-19 22:00:57 +0500

    home pageni fronted va routelari tayyor

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//home page
+Route::get('/', 'App\Http\Controllers\SiteController@home')->name('home');
